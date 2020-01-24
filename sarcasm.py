390e865de49f70ffb5fc135cde7760234a232e5e
Anthony George <anthony.george@tcnbroadcasting.com>
2020-01-24 14:53:38 -0700

    Add enough stuff to fulfill v1

--- sarcasm.py
+++ sarcasm.py
@@ -1,9 +1,13 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 import random
+import pyperclip
+import argparse
 
-def formatMessage(message):
+def formatMessage(args):
+    setupCLI()
     sarcastic = ""
-    for word in message.split(" "):
+
+    for word in args.message.split(" "):
         for char in word:
             cap = random.randrange(2)
             if cap == 0:
@@ -11,8 +15,23 @@ def formatMessage(message):
             else:
                 sarcastic += char.upper()
         sarcastic += " "
-    print(sarcastic)
+
+    if args.output:
+        print(sarcastic)
+
+    if args.copy:
+        pyperclip.copy(sarcastic)
+
+def setupCLI():
+    parser = argparse.ArgumentParser(description="Format text to a sARcaStIc FoNT.")
+    parser.add_argument("message", type=str, help="The message you want to format")
+    parser.add_argument("-c", "--copy", dest="copy", action="store_true", help="Copy output to clipboard")
+    parser.add_argument("-s", "--silent", dest="output", action="store_false", help="Silence output to std out")
+    args = parser.parse_args()
+
+    return args
 
 if __name__ == "__main__":
     import sys
-    formatMessage(sys.argv[1])
+    args = setupCLI()
+    formatMessage(args)
